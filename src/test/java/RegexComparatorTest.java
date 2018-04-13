3a098cdb519d932c9ff4cdacfdbfa32b1fdd34cc
amuniz <amuniz@cloudbees.com>
2018-04-13 18:19:57 +0200

    Improved DFA#equals to properly check equivalent transition graphs

--- src/test/java/RegexComparatorTest.java
+++ src/test/java/RegexComparatorTest.java
@@ -9,27 +9,33 @@ public class RegexComparatorTest {
     @Test
     public void basicComparisons() {
         // /A/**/* vs /A/B/C
-        assertTrue(RegexComparator.isContained("/A/(.)+", "/A/B/C"));
+        assertTrue(RegexComparator.contains("/A/(.)+", "/A/B/C"));
 
         // /A/**/D vs /A/**/B/D
-        assertTrue(RegexComparator.isContained("/A/(.)+/D", "/A/(.)+/B/D"));
+        assertTrue(RegexComparator.contains("/A/(.)+/D", "/A/(.)+/B/D"));
 
         // /A/**/D vs /A/**/B/C
-        assertFalse(RegexComparator.isContained("/A/(.)+/D", "/A/(.)+/B/C"));
+        assertFalse(RegexComparator.contains("/A/(.)+/D", "/A/(.)+/B/C"));
 
         // /A/B/C vs /A/B/C/D
-        assertFalse(RegexComparator.isContained("/A/B/C", "/A/B/C/D"));
+        assertFalse(RegexComparator.contains("/A/B/C$", "/A/B/C/D$"));
 
         // /A/B/C/* vs /A/B/C/D
-        assertTrue(RegexComparator.isContained("/A/B/C/[^/]+", "/A/B/C/D"));
+        assertTrue(RegexComparator.contains("/A/B/C/[^/]+", "/A/B/C/D"));
 
-        // /A/B/C/* vs /A/B/C/D/E
-        assertFalse(RegexComparator.isContained("/A/B/C/[^/]+", "/A/B/C/D/E"));
+        // /A/B/C/* vs /A/B/C/D/*
+        assertFalse(RegexComparator.contains("/A/B/C/[^/]+$", "/A/B/C/D/(.)+$"));
 
         // /**/* vs /A/B/C/D/E
-        assertTrue(RegexComparator.isContained("/(.)*", "/A/B/C/D/E"));
+        assertTrue(RegexComparator.contains("/(.)*", "/A/B/C/D/E"));
 
         // /**/C/* vs /**/*
-        assertFalse(RegexComparator.isContained("/(.)+/C/[^/]+", "/(.)*"));
+        assertFalse(RegexComparator.contains("/(.)+/C/[^/]+", "/(.)*"));
+
+        // **/B/* vs /A/B/*
+        assertTrue(RegexComparator.contains("/(.)+/B/[^/]+", "/A/B/[^/]+"));
+
+        // **/* vs A/B/*
+        assertTrue(RegexComparator.contains("/(.)*", "/A/B/[^/]+"));
     }
 }
